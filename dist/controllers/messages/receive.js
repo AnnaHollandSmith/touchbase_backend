90e423d129dac627acdf8db7f2eaa765066180e2
joestephens <joe.stephens@live.co.uk>
2017-10-29 12:17:53 +0000

    heroku build

--- dist/controllers/messages/receive.js
+++ dist/controllers/messages/receive.js
@@ -14,6 +14,10 @@ var _Config = require('../../models/Config');
 
 var _Config2 = _interopRequireDefault(_Config);
 
+var _Journey = require('../../models/Journey');
+
+var _Journey2 = _interopRequireDefault(_Journey);
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 var receive = function receive(req, res, next) {
@@ -22,72 +26,84 @@ var receive = function receive(req, res, next) {
 
   content = content.replace('TOUCHBASE ', '').toLowerCase();
 
-  switch (content) {
-    case 'extend':
-      console.log('extend');
-      (0, _helpers.extendJourney)(from).then(function (response) {
-        res.send(200);
-        next();
-      }).catch(function () {
-        res.send(200);
-        next();
-      });
-      break;
-    case 'home':
-      console.log('home');
-      (0, _helpers.terminateJourney)(from).then(function (response) {
-        res.send(200);
-        next();
-      }).catch(function () {
-        res.send(200);
-        next();
-      });
-      break;
-    case 'register':
-      console.log('register');
-      send999Sms(from, 'After reading ALL this message, SEND THE WORD \'' + process.env.KEYWORD + 'YES\' TO ' + process.env.NUMBER + ' TO COMPLETE YOUR REGISTRATION - otherwise your phone isn\'t registered. In an emergency, you will know your message has been received ONLY when you get a reply from an emergency service; until then try other methods. Full details, Terms & Conditions are available at www.emergencysms.org.uk').then(function () {
-        res.send(200);
-        next();
-      });
-      break;
-    case 'yes':
-      _Config2.default.findOne({ key: 'is999Registered' }).then(function (configVar) {
-        var registerPromise = new Promise(function (resolve, reject) {
-          if (!configVar) {
-            var newConfig = new _Config2.default({
-              key: 'is999Registered',
-              value: true
-            });
+  if (content.includes('escalate')) {
+    var reference = Number(content.replace('escalate ', ''));
 
-            newConfig.save(function (error, savedConfigVar) {
-              if (error) {
-                console.log(error);
-                reject(error);
-              }
-
-              resolve();
-            });
-          } else {
-            _Config2.default.update({ _id: configVar._id, key: 'is999Registered' }, { $set: { value: true } }).then(function () {
-              resolve();
-            }).catch(reject);
-          }
+    _Journey2.default.findOne({ reference: reference }).then(function (journey) {
+      send999Sms(process.env.NUMBER, process.env.KEYWORD + 'Police. Person Reported Missing. Last seen ' + journey.start + '. Mobile number ' + journey.mobileNumber + '. Last known coordinates ' + journey.origin.lat + ', ' + journey.origin.lng + '. Heading towards ' + journey.destination.lat + ', ' + journey.destination.lng);
+    });
+  } else {
+    switch (content) {
+      case 'extend':
+        console.log('extend');
+        (0, _helpers.extendJourney)(from).then(function (response) {
+          res.send(200);
+          next();
+        }).catch(function () {
+          res.send(200);
+          next();
         });
+        break;
+      case 'home':
+        console.log('home');
+        (0, _helpers.terminateJourney)(from).then(function (response) {
+          res.send(200);
+          next();
+        }).catch(function () {
+          res.send(200);
+          next();
+        });
+        break;
+      case 'register':
+        console.log('register');
+        send999Sms(from, 'After reading ALL this message, SEND THE WORD \'' + process.env.KEYWORD + 'YES\' TO ' + process.env.NUMBER + ' TO COMPLETE YOUR REGISTRATION - otherwise your phone isn\'t registered. In an emergency, you will know your message has been received ONLY when you get a reply from an emergency service; until then try other methods. Full details, Terms & Conditions are available at www.emergencysms.org.uk').then(function () {
+          res.send(200);
+          next();
+        });
+        break;
+      case 'yes':
+        _Config2.default.findOne({ key: 'is999Registered' }).then(function (configVar) {
+          var registerPromise = new Promise(function (resolve, reject) {
+            if (!configVar) {
+              var newConfig = new _Config2.default({
+                key: 'is999Registered',
+                value: true
+              });
 
-        registerPromise.then(function () {
-          send999Sms(from, 'Your telephone number is registered with the emergencySMS Service. Please don\'t reply to this message. For more information go to http://emergencySMS.org.uk').then(function () {
-            res.send(200);
-            next();
-          }).catch(function (error) {
-            return console.log(error);
+              newConfig.save(function (error, savedConfigVar) {
+                if (error) {
+                  console.log(error);
+                  reject(error);
+                }
+
+                resolve();
+              });
+            } else {
+              _Config2.default.update({ _id: configVar._id, key: 'is999Registered' }, { $set: { value: true } }).then(function () {
+                resolve();
+              }).catch(reject);
+            }
+          });
+
+          registerPromise.then(function () {
+            send999Sms(from, 'Your telephone number is registered with the emergencySMS Service. Please don\'t reply to this message. For more information go to http://emergencySMS.org.uk').then(function () {
+              res.send(200);
+              next();
+            }).catch(function (error) {
+              return console.log(error);
+            });
           });
         });
-      });
 
-      break;
-    default:
-      console.log('unhandled');
-      res.send('unhandled');
+        break;
+      default:
+        if (content.includes('Police.')) {
+          console.log('EMERGENCY REQUEST RECEIVED:' + content);
+        } else {
+          console.log('unhandled');
+          res.send('unhandled');
+        }
+    }
   }
 };
 
--- src/controllers/messages/receive.js
+++ src/controllers/messages/receive.js
@@ -1,6 +1,7 @@
 import request from 'request-promise'
 import { extendJourney, terminateJourney } from '../../helpers'
 import Config from '../../models/Config'
+import Journey from '../../models/Journey'
 
 const receive = (req, res, next) => {
   const from = req.params.from
@@ -8,22 +9,18 @@ const receive = (req, res, next) => {
 
   content = content.replace('TOUCHBASE ', '').toLowerCase()
 
-  switch (content) {
-    case 'extend':
-      console.log('extend')
-      extendJourney(from)
-        .then(response => {
-          res.send(200)
-          next()
-        })
-        .catch(() => {
-          res.send(200)
-          next()
-        })
-      break
-    case 'home':
-      console.log('home')
-      terminateJourney(from)
+  if (content.includes('escalate')) {
+    const reference = Number(content.replace('escalate ', ''))
+
+    Journey.findOne({reference})
+      .then(journey => {
+        send999Sms(process.env.NUMBER, `${process.env.KEYWORD}Police. Person Reported Missing. Last seen ${journey.start}. Mobile number ${journey.mobileNumber}. Last known coordinates ${journey.origin.lat}, ${journey.origin.lng}. Heading towards ${journey.destination.lat}, ${journey.destination.lng}`)
+      })
+  } else {
+    switch (content) {
+      case 'extend':
+        console.log('extend')
+        extendJourney(from)
           .then(response => {
             res.send(200)
             next()
@@ -32,65 +29,82 @@ const receive = (req, res, next) => {
             res.send(200)
             next()
           })
-      break
-    case 'register':
-      console.log('register')
-      send999Sms(from, `After reading ALL this message, SEND THE WORD '${process.env.KEYWORD}YES' TO ${process.env.NUMBER} TO COMPLETE YOUR REGISTRATION - otherwise your phone isn't registered. In an emergency, you will know your message has been received ONLY when you get a reply from an emergency service; until then try other methods. Full details, Terms & Conditions are available at www.emergencysms.org.uk`)
-        .then(() => {
-          res.send(200)
-          next()
-        })
-      break
-    case 'yes':
-      Config.findOne({ key: 'is999Registered' })
-        .then(configVar => {
-          const registerPromise = new Promise((resolve, reject) => {
-            if (!configVar) {
-              const newConfig = new Config({
-                key: 'is999Registered',
-                value: true
-              })
+        break
+      case 'home':
+        console.log('home')
+        terminateJourney(from)
+            .then(response => {
+              res.send(200)
+              next()
+            })
+            .catch(() => {
+              res.send(200)
+              next()
+            })
+        break
+      case 'register':
+        console.log('register')
+        send999Sms(from, `After reading ALL this message, SEND THE WORD '${process.env.KEYWORD}YES' TO ${process.env.NUMBER} TO COMPLETE YOUR REGISTRATION - otherwise your phone isn't registered. In an emergency, you will know your message has been received ONLY when you get a reply from an emergency service; until then try other methods. Full details, Terms & Conditions are available at www.emergencysms.org.uk`)
+          .then(() => {
+            res.send(200)
+            next()
+          })
+        break
+      case 'yes':
+        Config.findOne({ key: 'is999Registered' })
+          .then(configVar => {
+            const registerPromise = new Promise((resolve, reject) => {
+              if (!configVar) {
+                const newConfig = new Config({
+                  key: 'is999Registered',
+                  value: true
+                })
 
-              newConfig.save((error, savedConfigVar) => {
-                if (error) {
-                  console.log(error)
-                  reject(error)
-                }
+                newConfig.save((error, savedConfigVar) => {
+                  if (error) {
+                    console.log(error)
+                    reject(error)
+                  }
 
-                resolve()
-              })
-            } else {
-              Config.update({ _id: configVar._id, key: 'is999Registered' }, { $set: { value: true } })
+                  resolve()
+                })
+              } else {
+                Config.update({ _id: configVar._id, key: 'is999Registered' }, { $set: { value: true } })
+                .then(() => {
+                  resolve()
+                })
+                .catch(reject)
+              }
+            })
+
+            registerPromise
               .then(() => {
-                resolve()
+                send999Sms(from, `Your telephone number is registered with the emergencySMS Service. Please don't reply to this message. For more information go to http://emergencySMS.org.uk`)
+                  .then(() => {
+                    res.send(200)
+                    next()
+                  })
+                  .catch(error => console.log(error))
               })
-              .catch(reject)
-            }
           })
 
-          registerPromise
-            .then(() => {
-              send999Sms(from, `Your telephone number is registered with the emergencySMS Service. Please don't reply to this message. For more information go to http://emergencySMS.org.uk`)
-                .then(() => {
-                  res.send(200)
-                  next()
-                })
-                .catch(error => console.log(error))
-            })
-        })
-
-      break
-    default:
-      console.log('unhandled')
-      res.send('unhandled')
+        break
+      default:
+        if (content.includes('Police.')) {
+          console.log('EMERGENCY REQUEST RECEIVED:' + content)
+        } else {
+          console.log('unhandled')
+          res.send('unhandled')
+        }
+    }
   }
 }
 
 const send999Sms = (mobileNumber, message) => {
   return new Promise((resolve, reject) => {
     request.post(`https://api.clockworksms.com/http/send.aspx?key=${process.env.CLOCKWORK_API_KEY}&to=${mobileNumber}&content=${message}`)
-          .then(resolve)
-          .catch(reject)
+      .then(resolve)
+      .catch(reject)
   })
 }
 
